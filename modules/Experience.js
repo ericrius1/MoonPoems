ccc215ea222c2687f79bd0794919d2dce75b0bc1
Max Flowers <81179442+MaxFlowers333@users.noreply.github.com>
2022-01-13 19:59:34 -0600

    canvas

--- modules/Experience.js
+++ modules/Experience.js
@@ -14,14 +14,31 @@ class Experience {
     document.body.classList.add('ready')
 
     let canvasContainer = document.querySelector('div.canvas-container');
-    let $canvas = document.createElement('canvas')
-    $canvas.setAttribute('id', 'webgl_canvas');
-    $canvas.width = canvasContainer.clientWidth;
-    $canvas.height = canvasContainer.clientHeight
+    this.canvas = document.createElement('canvas')
+    this.canvas.setAttribute('id', 'webgl_canvas');
 
-    canvasContainer.appendChild($canvas);
+
+    canvasContainer.appendChild(this.canvas);
 
     let devicePixelRatio = window.devicePixelRatio;
+
+    this.canvas.setAttribute('tabindex', 0); // adds focus to canvas so keyboard events work
+    this.canvas.style.width = '100%';
+    this.canvas.style.height = '100%';
+    // this.canvas.focus();
+
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: this.canvas,
+      powerPreference: 'high-performance'
+    })
+
+    this.renderer.setSize(this.canvas.width * devicePixelRatio, this.canvas.height * devicePixelRatio, false);
+
+    this.camera = new THREE.PerspectiveCamera(70, this.canvas.width / this.canvas.height, .1, 100);
+    this.scene = new THREE.Scene();
+    this.scene.add(this.camera);
+    this.renderer.setClearColor(0x1101233, 1);
+    this.renderer.render(this.scene, this.camera)
 
   }
 }
