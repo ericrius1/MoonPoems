9988255ae0b382d490fbbe5f3c1947fb44b7e3b2
Max Flowers <81179442+MaxFlowers333@users.noreply.github.com>
2022-01-13 22:26:33 -0600

    sync material with css

--- modules/Experience.js
+++ modules/Experience.js
@@ -1,4 +1,5 @@
 import * as THREE from '../libs/three/three.module.js'
+import anime from '../libs/anime.es.js'
 
 let instance = null
 
@@ -25,19 +26,38 @@ class Experience {
     this.canvas.setAttribute('tabindex', 0); // adds focus to canvas so keyboard events work
     this.canvas.style.width = '100%';
     this.canvas.style.height = '100%';
+    this.canvas.width = canvasContainer.clientWidth;
+    this.canvas.height = canvasContainer.clientHeight;
     // this.canvas.focus();
 
     this.renderer = new THREE.WebGLRenderer({
+      // antialias: true,
       canvas: this.canvas,
       powerPreference: 'high-performance'
     })
 
+    this.renderer.outputEncoding = THREE.sRGBEncoding
     this.renderer.setSize(this.canvas.width * devicePixelRatio, this.canvas.height * devicePixelRatio, false);
 
+
     this.camera = new THREE.PerspectiveCamera(70, this.canvas.width / this.canvas.height, .1, 100);
+    this.camera.position.z += 10;
+    // this.camera.updateProjectionMatrix()
+    this.camera.lookAt(new THREE.Vector3())
     this.scene = new THREE.Scene();
     this.scene.add(this.camera);
-    this.renderer.setClearColor(0x1101233, 1);
+    this.renderer.setClearColor(0x1101222, 1);
+
+    this.hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+
+    this.scene.add(this.hemiLight)
+
+    let sphereGeo = new THREE.SphereGeometry(1, 30, 30);
+    let mat = new THREE.MeshBasicMaterial();
+    mat.color.setHex(0x5A87FF).convertSRGBToLinear();
+    this.mesh = new THREE.Mesh(sphereGeo, mat)
+    this.scene.add(this.mesh);
+
     this.renderer.render(this.scene, this.camera)
 
   }
